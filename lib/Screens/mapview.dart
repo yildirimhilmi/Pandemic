09a29031b92aff8e40462e2070936d984eb34c45
hilm yildirim <MY_NAME@example.com>
2021-01-13 21:28:02 +0300

    map view completely changed plase look mapview.dart

--- lib/Screens/mapview.dart
+++ lib/Screens/mapview.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:latlong/latlong.dart' as location;
+
+String url =
+    "https://api.mapbox.com/styles/v1/hcy043/ckjvq77lw01ub17pupqcjzgtr/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaGN5MDQzIiwiYSI6ImNranZvdWJ0dDBhZmQybmxzMG11ZWQ3bnQifQ.XUx00U-uMD-CA50qU5aN9w";
 
 class MapView extends StatefulWidget {
   @override
@@ -16,9 +21,12 @@ class _MapViewState extends State<MapView> {
           title: const Text('Flutter Maps example app'),
         ),
         body: FlutterMap(
-          options: new MapOptions(minZoom: 10.0),
+          options: new MapOptions(
+            center: new location.LatLng(41.015137, 28.979530),
+            minZoom: 10,
+          ),
           layers: [
-            new TileLayerOptions(urlTemplate: "https."),
+            new TileLayerOptions(urlTemplate: url),
           ],
         ),
       ),
