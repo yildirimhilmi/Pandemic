818fbf693bd8695b08f18d58f536e8ea14eb9fd9
Hilmi cihan <hilmi0043@gmail.com>
2021-01-13 18:52:40 +0300

    first commit

---
+++ lib/generated_plugin_registrant.dart
@@ -0,0 +1,15 @@
+//
+// Generated file. Do not edit.
+//
+
+import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
+import 'package:video_player_web/video_player_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(Registrar registrar) {
+  GoogleMapsPlugin.registerWith(registrar);
+  VideoPlayerPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
