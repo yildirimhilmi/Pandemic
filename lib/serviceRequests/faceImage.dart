725b7ee5d8e9741e880f51d9e036554daeba9b9a
Hilmi cihan <hilmi0043@gmail.com>
2021-04-20 23:45:17 +0300

    face image class added

--- lib/serviceRequests/faceImage.dart
+++ lib/serviceRequests/faceImage.dart
@@ -1,1 +1,25 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
+class FaceImage {
+  final int id;
+  final String image_path;
+  final bool is_live;
+  final int scene_id;
+  final DateTime time;
+  final int mask;
+
+  FaceImage(this.id, this.image_path, this.is_live, this.scene_id, this.time,
+      this.mask);
+  factory FaceImage.fromMap(Map<String, dynamic> json) {
+    return FaceImage(
+      json['id'],
+      json['image_path'],
+      json['is_live'],
+      json['scene_id'],
+      json['timestamp'],
+      json['mask'],
+    );
+  }
+}
