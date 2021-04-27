efc658bfd4d17667b9664d9acd952a8c8691877c
Hilmi cihan <hilmi0043@gmail.com>
2021-04-27 22:49:43 +0300

    3 table json object added.

---
+++ lib/serviceRequests/location.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:ffi';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
+
+class Location {
+  final int id;
+  final String country;
+  final String city;
+  final String street;
+  final Float latitude;
+  final Float longitude;
+
+  Location(this.id, this.country, this.city, this.street, this.latitude,
+      this.longitude);
+  factory Location.fromMap(Map<String, dynamic> json) {
+    return Location(
+      json['id'],
+      json['country'],
+      json['city'],
+      json['street'],
+      json['latitude'],
+      json['longitude'],
+    );
+  }
+}
