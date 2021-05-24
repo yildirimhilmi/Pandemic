c739da6ab1323ed8ab9fe8139ed5a844458c67ae
ivibrahim <ivibrahim@hotmail.com>
2021-05-24 23:23:43 +0300

    cc

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,53 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps/google_maps.dart' hide Icon;
 import 'dart:html';
 import 'package:my_website_project/Screens/mapview.dart';
+import 'package:my_website_project/chart/chartPage.dart';
+import 'package:my_website_project/screenComponents/AppBar.dart';
 import 'package:my_website_project/screenComponents/cardView.dart';
+import 'package:my_website_project/screenComponents/sideMenu.dart';
+
+import 'SignInOut/LogIn.dart';
+import 'SignInOut/SignIn.dart';
+
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Pandemic Tracking',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home:MyApp()
+      //home:ChartPage(),
+    //home:SignIn(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Pandemic Tracking',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: Scaffold(
-          body: Stack(
-            children: [
-              MapView(),
-              Positioned(
-                left: 50,
-                top: 50,
-                child: CardView(),
-              ),
-            ],
+    return
+            Scaffold(
+
+          drawer: NavDrawer(),
+          appBar: newAppBar(context),
+          body: Container(
+            padding: EdgeInsets.all(0),
+            child: Stack(
+              children: [
+                MapView(),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: CardView(),
+                ),
+                //
+              ],
+            ),
           ),
-        ));
+        );
   }
 }
--- lib/screenComponents/cardView.dart
+++ lib/screenComponents/cardView.dart
@@ -1,12 +1,15 @@
+import 'dart:convert';
 import 'dart:ui';
 import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:latlong/latlong.dart' as location;
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:my_website_project/chart/chartPage.dart';
+import 'package:my_website_project/screenComponents/RiskAPI.dart';
 import 'package:my_website_project/screenComponents/locations.dart';
-import 'package:my_website_project/screenComponents/videoPlayer.dart';
 import 'package:percent_indicator/percent_indicator.dart';
-import 'package:cached_video_player/cached_video_player.dart';
+import 'package:http/http.dart' as http;
+
 
 MapController globalMapController = MapController();
 location.LatLng istanbul2 = new location.LatLng(41.015137, 28.979530);
@@ -19,6 +22,44 @@ class CardView extends StatefulWidget {
 }
 
 class _CardViewState extends State<CardView> with TickerProviderStateMixin {
+
+  double riskPercentage=0.2;
+  void initState() {
+    super.initState();
+    updateRisk();
+  }
+
+  Future<List<RiskApi>> getRisk(int locID,DateTime startDate, DateTime endDate) async {
+    final response = await http.get("http://localhost:59138/risk/$locID/$startDate/$endDate");
+    if (response.statusCode == 200) {
+
+      List<RiskApi> jsonList;
+      jsonList=(json.decode(response.body) as List).map((i) =>
+          RiskApi.fromJson(i)).toList();
+      print(jsonList);
+      return jsonList;
+
+    } else {
+      throw Exception('Failed to load');
+    }
+  }
+  void updateRisk() async
+  {
+    //DateTime endDate=DateTime.now();
+    DateTime endDate=DateTime.parse("2021-05-06");
+    DateTime startDate=DateTime.parse("2021-03-06");
+    //DateTime startDate= endDate.subtract(new Duration(hours: 23,minutes: 59));
+    print(startDate);
+    print(endDate);
+    List<RiskApi> riskList= await getRisk(27,startDate,endDate);
+
+    setState(() {
+      riskPercentage=riskList[0].percentage;
+    });
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -38,30 +79,56 @@ class _CardViewState extends State<CardView> with TickerProviderStateMixin {
               child: Padding(
                 padding: const EdgeInsets.all(10.0),
                 child: Text(
-                  "Misir Çarşısı",
+                  "Mısır Çarşısı",
                   style: TextStyle(fontSize: 20),
                 ),
               ),
             ),
             GestureDetector(
-              child: Container(
-                child: Icon(Icons.play_arrow),
+              child: Tooltip(
+                message: "Shows video of the location",
+                child: Container(
+                  child: Icon(Icons.play_arrow),
+                ),
               ),
               onTap: () {
                 AwesomeDialog(
+                  dialogBackgroundColor: Colors.blueAccent,
+                  dialogType: DialogType.NO_HEADER,
                   context: context,
-                  width: 500,
+                  width: 720,
                   buttonsBorderRadius: BorderRadius.all(Radius.circular(2)),
                   headerAnimationLoop: false,
                   animType: AnimType.BOTTOMSLIDE,
-                  title: 'INFO',
                   body: Container(
-                    child: Text("videos will be show here"),
+                    child: Column(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(20),
+                          child: Image.asset("images/ed.png"),
+                        ),
+                        ElevatedButton(
+                            style: ButtonStyle(
+                              overlayColor:MaterialStateProperty.all(Colors.greenAccent) ,
+                                backgroundColor:
+                                    MaterialStateProperty.all(Colors.white)),
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => ChartPage()));
+                            },
+                            child: Text(
+                              "Risk Analysis of the Location",
+                              style: TextStyle(color: Colors.black,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 2),
+                            ))
+                      ],
+                    ),
                   ),
                   showCloseIcon: true,
-                  btnCancelOnPress: () {},
-                  btnOkOnPress: () {},
-                )..show();
+                ).show();
               },
             ),
             SizedBox(width: 20),
@@ -82,17 +149,23 @@ class _CardViewState extends State<CardView> with TickerProviderStateMixin {
                   this,
                 );
               },
-              child: Container(
-                child: Icon(Icons.my_location),
+              child: Tooltip(
+                message: "Click to zoom into the location\n Double click to zoom out",
+                child: Container(
+                  child: Icon(Icons.my_location),
+                ),
               ),
             ),
             SizedBox(width: 20),
-            CircularPercentIndicator(
-              radius: 60.0,
-              lineWidth: 10.0,
-              percent: 0.7,
-              center: new Text("70%"),
-              progressColor: Colors.green,
+            Tooltip(
+              message: "Percentage of Wearing Mask Properly",
+              child: CircularPercentIndicator(
+                radius: 60.0,
+                lineWidth: 10.0,
+                percent: 1-riskPercentage,
+                center: new Text((100-riskPercentage*100).toInt().toString()+"%"),
+                progressColor: riskPercentage<30? Colors.green:Colors.red,
+              ),
             ),
           ],
         ),
